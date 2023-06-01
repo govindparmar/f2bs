42cd8ba54815e35c30170a36efd11d0cc893fbe7
Govind Parmar <0.govindparmar@gmail.com>
2023-06-01 17:45:08 -0600

    Rename complete

--- f2bs/getsettings.c
+++ f2bs/getsettings.c
@@ -9,21 +9,21 @@ SETTINGS WINAPI GetSettings(_In_ INT nArgc, _In_reads_(nArgc) WCHAR *pArgv[])
 	HRESULT hr;
 
 	ZeroMemory(&retVal, sizeof(SETTINGS));
-	retVal.nSpaces = 1;
+	retVal.uSpaces = 1;
 	for (i = 1; i < nArgc; i++)
 	{
 		if (L'-' == pArgv[i][0] || L'/' == pArgv[i][0] && wcslen(pArgv[i]) >= 2U)
 		{
 			retVal.fHexSyntax = retVal.fHexSyntax || (towupper(pArgv[i][1]) == L'X');
 			retVal.fCommaSep = retVal.fCommaSep || (towupper(pArgv[i][1]) == L'C');
-			if (1 == retVal.nSpaces && towupper(pArgv[i][1]) == L'S')
+			if (1 == retVal.uSpaces && towupper(pArgv[i][1]) == L'S')
 			{
 				INT nOff = 2 + (L':' == pArgv[i][2]);
 				ulSpaces = wcstoul(&pArgv[i][nOff], NULL, 10);
 				hr = ULongToUInt8(ulSpaces, &uSpaces);
 				if (SUCCEEDED(hr))
 				{
-					retVal.nSpaces = uSpaces;
+					retVal.uSpaces = uSpaces;
 				}
 			}
 		}
